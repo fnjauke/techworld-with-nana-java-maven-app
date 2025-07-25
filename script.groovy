9946f8021c03160e6c25411dee53a998f717b4c4
fnjauke <fnjauke@gmail.com>
2025-07-25 22:32:31 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,6 +2,19 @@ def buildApp() {
     echo 'Building the Application'
 }
 
+def buildJar() {
+    echo "building the application..."
+    sh "mvn package"
+}
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo',passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker build -t fnjauke/demo-app:jma-2.0 ."
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh " docker push fnjauke/demo-app:jma-2.0"
+    }
+}
 
 def testApp() {
     echo 'Running tests...'
